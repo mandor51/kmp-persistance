aa26b40137aa90f08dca4d00faf321ae236391f2
mandor51 <mandor51@gmail.com>
2025-10-23 11:44:37 +0200

    Initial commit

---
+++ kmp-persistance/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+kotlin {
+    jvmToolchain(21)
+
+    androidTarget { publishLibraryVariants("release") }
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.coroutines.test)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.multiplatformSettings)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+
+        androidMain.dependencies {
+            implementation(libs.kotlinx.coroutines.android)
+        }
+
+        jvmMain.dependencies {
+            implementation(libs.kotlinx.coroutines.swing)
+        }
+
+    }
+
+    //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        compilations["main"].compileTaskProvider.configure {
+            compilerOptions {
+                freeCompilerArgs.add("-Xexport-kdoc")
+            }
+        }
+    }
+
+}
+
+android {
+    namespace = "com.mandor.kmp.persistance"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+//Publishing your Kotlin Multiplatform library to Maven Central
+//https://www.jetbrains.com/help/kotlin-multiplatform-dev/multiplatform-publish-libraries.html
+mavenPublishing {
+    publishToMavenCentral()
+    coordinates("com.mandor.kmp.persistance", "kmp-persistance", "1.0.0")
+
+    pom {
+        name = "kmp-persitance"
+        description = "Kotlin Multiplatform library"
+        url = "github url" //todo
+
+        licenses {
+            license {
+                name = "MIT"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+
+        developers {
+            developer {
+                id = "" //todo
+                name = "" //todo
+                email = "" //todo
+            }
+        }
+
+        scm {
+            url = "github url" //todo
+        }
+    }
+    if (project.hasProperty("signing.keyId")) signAllPublications()
+}
